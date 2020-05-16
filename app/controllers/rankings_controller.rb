3c3c4fd9cbaa77482342fbb5ad6b9bf48906141e
kento.nakamura <kentz.eva.a.r.xxx@gmail.com>
2020-05-16 06:32:13 +0000

    ranking view実装

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,5 @@
 class RankingsController < ApplicationController
   def index
-    @books_rank = Book.find(Userbook.group(:book_id).order('count(book_id) desc').limit(3).pluck(:book_id))
+    @books_rank = Book.find(Userbook.group(:book_id).order('count(book_id) desc').limit(10).pluck(:book_id))
   end
 end
