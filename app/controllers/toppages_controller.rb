de878686599b9fd803ef33378f313a3158cd6ea6
kento.nakamura <kentz.eva.a.r.xxx@gmail.com>
2020-05-16 08:11:23 +0000

    toppageにrankingbookの追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,5 +2,6 @@ class ToppagesController < ApplicationController
   def index
     @users = User.all.order(id: 'desc').limit(4)
     @books = Book.all.order(id: 'desc').limit(4)
+    @books_rank = Book.find(Userbook.group(:book_id).order('count(book_id) desc').limit(4).pluck(:book_id))
   end
 end
