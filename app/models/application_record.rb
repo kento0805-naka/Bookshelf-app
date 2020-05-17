d5d8d507dbdf535eb7f1dce933dd3244c4b7bddd
kento.nakamura <kentz.eva.a.r.xxx@gmail.com>
2020-05-17 09:07:55 +0000

    book show view の調整

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,9 @@ class ApplicationRecord < ActiveRecord::Base
   def register_book(book)
     self.userbooks.find_or_create_by(book_id: book.id)
   end
+  
+  def registered_book?(book)
+    self.books.include?(book)
+  end
+  
 end
