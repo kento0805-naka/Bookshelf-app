0c8432d812558aeeee5baa2f8001dd6daf5f92db
kento.nakamura <kentz.eva.a.r.xxx@gmail.com>
2020-06-02 07:52:08 +0000

    factory_bot_railsの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "名前とメールアドレスとパスワードがあれば登録できる" do
   
+    user = User.new(
+      id: 1,
+      name: 'kento',
+      email: 'example@gmail.com',
+      password: 'password',
+      created_at: '2020-05-12 11:35:55',
+      updated_at: '2020-05-12 11:35:55'
+      )
+    
+    expect(user).to be_valid
+  end
 end
